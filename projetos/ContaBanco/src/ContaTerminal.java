cc8345f6bd10a73f55d893fb403d31b16f8f0103
CaioPrimoDev <caioprimoedi@gmail.com>
2024-05-17 14:54:23 -0300

    alteração do projeto

--- projetos/ContaBanco/src/ContaTerminal.java
+++ projetos/ContaBanco/src/ContaTerminal.java
@@ -6,34 +6,33 @@ public class ContaTerminal {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         // Nome do cliente(String) + agência(String) + número da conta(int) + saldo(double);
-        System.out.println("Bem Vindo ao Banco Santander!\n\nGostario de proseguir com a criação da sua conta?\nPresione ENTER para continuar...");
+        System.out.println("\nBem Vindo ao Banco Santander!\n\nGostario de proseguir com a criação da sua conta?\nPresione ENTER para continuar...");
            scanner.nextLine();
 
-        System.out.println("Informe seu nome: ");
+        System.out.println("\nInforme seu nome: ");
            String nome = scanner.next();
 
-        System.out.println("Informe seu sobrenome: ");
+        System.out.println("\nInforme seu sobrenome: ");
            String sobrenome = scanner.next();
            String nomeCliente = nome + " " + sobrenome;
        
-        System.out.println("Informe sua Agência: ");
+        System.out.println("\nInforme sua Agência: ");
            String agencia = scanner.next();
            
-        System.out.println("Informe o número da sua conta: ");
+        System.out.println("\nInforme o número da sua conta: ");
            int numeroConta = scanner.nextInt();
 
            scanner.nextLine();
 
-        System.out.println("Parabens!\nSua conta foi criada com sucesso\n\nDigite ENTER para fazer seu primeiro deposito...");
+        System.out.println("\nParabens!\nSua conta foi criada com sucesso\n\nDigite ENTER para fazer seu primeiro deposito...");
            scanner.nextLine();
 
-        System.out.println("Informe o valor que deseja depositar: ");
+        System.out.println("\nInforme o valor que deseja depositar: ");
            double deposito = scanner.nextDouble();
            double saldo = 0;
-           saldo = saldo + deposito;
-
+           saldo = saldo + deposito;    
         
-           System.out.println("\n\nOlá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta "+  numeroConta + " e seu saldo " + saldo + " já está disponível para saque!\n\n");
+           System.out.println("\n\nOlá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta "+  numeroConta + " e o seu saldo de " + saldo + " já está disponível para saque!\n\n");
     }
     
 }
