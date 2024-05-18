2ee686d642652b1b1b0f2f8b5e2bcb3ba69822f2
CaioPrimoDev <caioprimoedi@gmail.com>
2024-05-18 14:44:09 -0300

    organizando

--- aulas/anatomia-de-classes/src/AnatomiaClasses.java
+++ aulas/anatomia-de-classes/src/AnatomiaClasses.java
@@ -1,5 +1,18 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class AnatomiaClasses {
+
+    public static void main(String [] args) {
+
+      String primeiroNome = "Gleyson"; //variavel
+      String segundoNome = "Sampaio"; //variavel
+      
+      String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+      System.out.println(nomeCompleto);
+
     }
-}
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+       return "Resultado do Metodo " + primeiroNome.concat(" ").concat(segundoNome);
+
+    }
+    
+    }
